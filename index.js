e5011634e338f1611980f868a955d4c3b2115caf
Ricardo Devis Agullo <ricardo.devisagullo@guestline.com>
2018-05-04 17:30:46 +0200

    add a rule for enforcing newline before a return

--- index.js
+++ index.js
@@ -7,10 +7,13 @@
 
 'use strict';
 
-/* As of a first version, we only extend the config of
- * eslint-config-react-app as it serves most of our cases.
- * We might however overwrite or add rules later.
- */
 module.exports = {
-  extends: ['react-app']
+  extends: ['react-app'],
+  rules: {
+    'padding-line-between-statements': [
+      'error',
+      // Always require blank lines before return statements
+      { blankLine: 'always', prev: '*', next: 'return' }
+    ]
+  }
 };
